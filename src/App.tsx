448647a9fdef587efa5c8f984885be3f45c72e17
tomicilija <ilijatomic17@gmail.com>
2022-11-22 20:49:52 +0100

    Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
 import { UpdateProvider } from './utils/UpdateContext';
 class App extends React.Component {
@@ -9,6 +10,7 @@ class App extends React.Component {
       <UpdateProvider>
         <BrowserRouter>
           <Navbar />
+          <Footer />
         </BrowserRouter>
       </UpdateProvider>);
   }
